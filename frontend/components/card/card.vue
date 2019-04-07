e4baf65b8cfb34b568c87869383392dc4985ba12
Richard Jarram <rjarram@me.com>
2019-04-07 14:19:07 -0400

    changes in web server yml

--- frontend/components/card/card.vue
+++ frontend/components/card/card.vue
@@ -25,16 +25,17 @@
             </i>
           </form>
 
+          <!-- Card Modal -->
+          <div class="card-modal">
+            <button>Delete</button>
+            <button>Don't Delete</button>
+          </div>
+
         </div>
       </div>
       <img v-bind:src="imageurl" />
     </div>
     <div class="proj-name">{{name}}</div>
-
-    <div class="card-modal">
-      <button>Delete</button>
-      <button>Don't Delete</button>
-    </div>
   </div>
 </template>
 <script src="./card.js"></script>
