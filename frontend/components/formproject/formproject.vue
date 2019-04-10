58ac4ddc657d52acca21d0a98cd1da9bd82b2733
Richard Jarram <rjarram@me.com>
2019-04-10 11:48:47 -0400

    scaffolded style for form page

--- frontend/components/formproject/formproject.vue
+++ frontend/components/formproject/formproject.vue
@@ -1,22 +1,37 @@
 <template>
-  <form v-bind:action="httpaction" method="post">
-    <input type="hidden" name="_method" v-bind:value="httpverb">
-    <input type="hidden" name="authenticity_token" v-bind:value="mycsrf">
+  <div class="form-container">
+    <form class="form"
+          v-bind:action="httpaction"
+          method="post">
+      <input type="hidden" name="_method" v-bind:value="httpverb">
+      <input type="hidden" name="authenticity_token" v-bind:value="mycsrf">
 
-    <label for="">Name</label>
-    <input type="text" name="project[name]">
+      <div class="form-line">
+        <label for="">Project Name</label>
+        <input type="text" name="project[name]">
+      </div>
 
-    <label for="">Description</label>
-    <input type="textarea" name="project[description]">
+      <div class="form-line">
+      <label for="">Description</label>
+        <input type="textarea" name="project[description]" cols="15" rows="10">
+      </div>
 
-    <label for="">Cover Image</label>
-    <input type="text" name="project[coverimage]">
+      <div class="form-line">
+        <label for="">Cover Image</label>
+        <input type="select" name="project[coverimage]">
+      </div>
 
-    <label for="">Upload Image</label>
-    <button>Select File</button>
+      <div class="form-line">
+        <label for="">Upload Image</label>
+        <input class="form-button-inverted" type="submit" value="Select File">
+      </div>
 
-    <input type="submit" value="Submit">
-  </form>
+      <div class="form-button-container">
+        <input class="form-button" type="submit" href="/admin" value="Discard"></input>
+        <input class="form-button" type="submit" value="Submit">
+      </div>
+    </form>
+  </div>
 </template>
 <script src="./formproject.js"></script>
 <style lang="scss" src="./formproject.scss"></style>
