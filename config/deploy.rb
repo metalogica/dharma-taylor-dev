9b1b6b3c7d8c28694dfb6ae3fafa78c1359a72fc
Richard Jarram <rjarram@me.com>
2019-04-29 17:00:04 -0400

    mod cap deploy dile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,9 @@
 # Change these
 server '18.130.97.77', port: 22, roles: [:web, :app, :db], primary: true
 
-# set :repo_url,        'git@bitbucket.org:nfecastro/dharma-taylor.git'
-set :repo_url,        'git@github.com:catonmat/dharma-taylor-dev.git' #Lailo Test Environment
+set :repo_url,        'git@bitbucket.org:nfecastro/dharma-taylor.git'
 set :application,     'dharma-taylor'
-# set :user,            'ubuntu'
-set :user,            'ubuntu' # Lailo Test Environment
+set :user,            'ubuntu'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
@@ -14,8 +12,7 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-# set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :deploy_to,       "/var/www/lailo-dharma-taylor" #Lailo Test Environment
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
